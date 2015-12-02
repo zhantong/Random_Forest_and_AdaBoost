4db4400b5db278c7fc9e29902100191a86c84d39
zhantong1994 <zhantong1994@163.com>
2015-12-02 19:20:04 +0800

    增加对连续型变量的支持
    不过测试发现错误率达到10%，不太确定是代码的问题还是数据本身问题

--- C4.5.py
+++ C4.5.py
@@ -64,10 +64,15 @@ class Node():
 		info_gains=[]
 		split_infos=[]
 		for feature_index in self.features_usable:
-			colomn=Node.data['colomns'][feature_index]['data']
-			info_e=info_entropy(colomn,Node.data['labels']['data'],self.locations)
-			info_gains.append(label_entropy-info_e)
-			split_infos.append(entropy(colomn,self.locations))
+			if Node.data['colomns'][feature_index]['is_discrete']:
+				colomn=Node.data['colomns'][feature_index]['data']
+				info_e=info_entropy(colomn,Node.data['labels']['data'],self.locations)
+				info_gain=label_entropy-info_e
+				split_info=entropy(colomn,self.locations)
+			else:
+				info_gain,split_info=numeric_info_entropy(label_entropy,Node.data['colomns'][feature_index],Node.data['labels']['data'],self.locations)
+			info_gains.append(info_gain)
+			split_infos.append(split_info)
 			#print(info_e,info_gains)
 		avg_split_info=sum(split_infos)/len(split_infos)
 		#print(self.features_usable,self.locations)
@@ -85,17 +90,31 @@ class Node():
 		if self.is_leaf:
 			return
 		self.feature_index=self.index_max_info_gain_ratio()
-		feature=Node.data['colomns'][self.feature_index]['data']
-		s=list(set(feature))
-		condition_num=len(s)
-		self.split_condition=s
 		features_usable=self.features_usable[:]
 		features_usable.remove(self.feature_index)
-		locations_list=[[] for x in s]
-		for index in self.locations:
-			locations_list[s.index(feature[index])].append(index)
-		self.children=[None]*condition_num
+		feature=Node.data['colomns'][self.feature_index]['data']
+		
+
+		if Node.data['colomns'][self.feature_index]['is_discrete']:
+			
+			s=list(set(feature))
+			condition_num=len(s)
+			self.split_condition=s
+			locations_list=[[] for x in s]
+			for index in self.locations:
+				locations_list[s.index(feature[index])].append(index)
+			
+		else:
+			condition_num=2
+			self.split_condition=Node.data['colomns'][self.feature_index]['threshold']
+			locations_list=[[],[]]
+			for index in self.locations:
+				if feature[index]<=self.split_condition:
+					locations_list[0].append(index)
+				else:
+					locations_list[1].append(index)
 		#print('inner:',self.depth,self.feature_index,self.split_condition,self.locations,self.features_usable)
+		self.children=[None]*condition_num
 		for i in range(condition_num):
 			self.children[i]=Node(locations_list[i],self,features_usable,self.depth+1)
 			self.children[i].do()
@@ -103,7 +122,13 @@ class Node():
 		#print(self.depth)
 		if self.is_leaf:
 			return self.label
-		split_index=self.split_condition.index(sample[self.feature_index])
+		if Node.data['colomns'][self.feature_index]['is_discrete']:
+			split_index=self.split_condition.index(sample[self.feature_index])
+		else:
+			if sample[self.feature_index]<=self.split_condition:
+				split_index=0
+			else:
+				split_index=1
 		return self.children[split_index].get(sample)
 def entropy(data,locations):
 	s=set([data[index] for index in locations])
@@ -139,6 +164,34 @@ def info_entropy(feature,labels,locations):
 	for c in f_count.values():
 		result+=c['count']/f_length*entropy(labels,c['locations'])
 	return result
+def numeric_info_entropy(label_entropy,colomn,labels,locations):
+	temp=sorted([(colomn['data'][index],labels[index]) for index in locations],key=lambda x:x[0])
+	data=[x[0] for x in temp]
+	label=[x[1] for x in temp]
+	length=len(data)
+	label_set=list(set(label))
+	lab=label_set[0]
+	count1=label.count(lab)
+	igrs=[]
+	for i in range(1,length):
+		count2=label[0:i].count(lab)
+		t1=count2/i
+		t2=(count1-count2)/(length-i)
+		d1=i/length
+		ie=0
+		#print(i,length,count1,count2,t1,t2)
+		if t1!=0 and t1!=1:
+			ie+=d1*(-t1*math.log2(t1)-(1-t1)*math.log2(1-t1))
+		if t2!=0 and t2!=1:
+			ie+=(1-d1)*(-t2*math.log2(t2)-(1-t2)*math.log2(1-t2))
+		ig=label_entropy-ie
+		si=-d1*math.log2(d1)-(1-d1)*math.log2(1-d1)
+		igr=ig/si
+		igrs.append((ig,si,i-1,igr))
+	m=max(igrs,key=lambda x:x[3])
+	colomn['threshold']=data[m[2]]
+	return m[0],m[1]
+
 class C_4_point_5():
 	def __init__(self,file_path):
 		self.data={
@@ -185,9 +238,12 @@ class C_4_point_5():
 		root.do()
 		#root.test()
 		#print(root.get([4,1,3,0,1,0,1,4,1]))
+		count=0
 		for line,real_label in  zip(Node.data['rows'],Node.data['labels']['data']):
 			label=root.get(line)
-			print(label,real_label)
+			if label!=real_label:
+				count+=1
+		print(count)
 if __name__=='__main__':
-	c=C_4_point_5('data/breast-cancer-assignment5.txt')
+	c=C_4_point_5('data/german-assignment5.txt')
 	c.do()
